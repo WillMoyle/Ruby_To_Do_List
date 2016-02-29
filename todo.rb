76dbe8f1b5267c47c793b01bdd33c079318abf3b
William Moyle <william.moyle14@imperial.ac.uk>
2016-02-29 12:12:03 -0500

    Basic file and class structure established

---
+++ todo.rb
@@ -0,0 +1,38 @@
+# Command Line To Do List App
+# Author: Will Moyle
+# Last Updated: 29 Feb 2016
+
+# List class
+
+class List
+
+    def initialize
+
+    end
+
+    # add task to list
+
+    # show all tasks
+
+    # read a task from a file
+
+    # write a list to a file
+
+    # delete a task
+
+    # update a task
+
+end
+
+
+# Task class
+
+class Task
+
+    def initialize
+
+    end
+
+end
+
+my_list = List.new
